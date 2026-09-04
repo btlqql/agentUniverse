e2ec52886dda7fc394667277aed5613f0d8d0ca7
Yue Wang <1939455790@qq.com>
2026-09-04 23:54:06 +0800

    docs(base): document missing docstrings in env_util.py

--- agentuniverse/base/util/env_util.py
+++ agentuniverse/base/util/env_util.py
@@ -10,5 +10,14 @@ import os
 
 
 def get_from_env(env_key: str) -> str:
+    """Read the value of an environment variable.
+
+    Args:
+        env_key: The name of the environment variable to look up.
+
+    Returns:
+        The environment variable's value if it is set and non-empty,
+        otherwise None.
+    """
     if env_key in os.environ and os.environ[env_key]:
         return os.environ[env_key]
