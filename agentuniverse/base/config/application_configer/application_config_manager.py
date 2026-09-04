8a27160863d831957ffd05f10d4b564417e3a5aa
Yue Wang <1939455790@qq.com>
2026-09-05 07:50:25 +0800

    style(base): strip trailing whitespace in application_config_manager.py

--- agentuniverse/base/config/application_configer/application_config_manager.py
+++ agentuniverse/base/config/application_configer/application_config_manager.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 
 # @Time    : 2024/3/14 11:54
-# @Author  : jerry.zzw 
+# @Author  : jerry.zzw
 # @Email   : jerry.zzw@antgroup.com
 # @FileName: application_config_manager.py
 from typing import Optional
